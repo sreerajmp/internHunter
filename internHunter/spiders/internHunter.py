26d26407ff6092f7192a8d40a8ca21a18e295c22
sreeraj <sreerajmp1996@gmail.com>
2021-02-22 13:51:12 +0530

    argument for crawling

--- internHunter/spiders/internHunter.py
+++ internHunter/spiders/internHunter.py
@@ -4,12 +4,18 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name = "internshala"
 
-    def start_requests(self):
-        urls = [
-            'https://internshala.com/internships'
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    #     urls = [
+    #         'https://internshala.com/internships'
+    #     ]
+    #     for url in urls:
+    #         yield scrapy.Request(url=url, callback=self.parse)
+    # def start_requests(self):
+    #     yield scrapy.Request(f'https://internshala.com/internships/keyword-{self.category}')   
+    def __init__(self, category=None, *args, **kwargs):
+        super(QuotesSpider, self).__init__(*args, **kwargs)
+        print("cat:",category)
+        self.start_urls = [f'https://internshala.com/internships/keywords-{category}']    
 
     def parse(self, response):
         page = response.url.split("/")[-2]
